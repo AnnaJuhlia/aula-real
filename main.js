3d5d9cd1d612d50d90cabf1ddebcd801008a2ce2
AnnaJuhlia <julia.silva.anna@escola.pr.gov.br>
2024-10-03 11:32:22 -0300

    plp

---
+++ main.js
@@ -0,0 +1,7 @@
+// alert('Anna e Anna');
+// let num1=parseInt(prompt('qual o numero'));
+// let num2=parseInt(prompt('qual o numero'));
+// let num3=parseInt(prompt('qual o numero'));
+// function media (x,y,z){return(x,y,z)/3;}
+// document.write(media(num1,num2,num3));
+
