c8de9c0ced8532169535152c783c7a72a6b374c4
AnnaJuhlia <julia.silva.anna@escola.pr.gov.br>
2024-10-03 12:28:51 -0300

    gg

--- main.js
+++ main.js
@@ -5,17 +5,20 @@
 // function media (x,y,z){return(x,y,z)/3;}
 // document.write(media(num1,num2,num3));
 
-let estados= ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+// let estados= ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 
-let i=0;
-while(i<estados.length){
-    console.log(i);
-    console.log(estados[i]);
-    i++;
-    for (let j = 0; j < estados.length; j++) {
-        console.log(j);
-        console.log(estados[j]);
+// let i=0;
+// while(i<estados.length){
+//     console.log(i);
+//     console.log(estados[i]);
+//     i++;
+//     for (let j = 0; j < estados.length; j++) {
+//         console.log(j);
+//         console.log(estados[j]);
         
-    }
+//     }
 
-}
+// }
+let idade=60;
+if(idade>17){console.log('pode dirigir');}
+else{console.log('ilegal dirigir');}
